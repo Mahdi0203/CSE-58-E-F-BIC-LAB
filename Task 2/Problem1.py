c5cfb5c0d0a220b5293d46bf1095440637b24e4b
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2026-06-09 15:20:06 +0600

    Count occurrences of pattern in text

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,11 @@
+text = input()
+patern = input()
 
+pat_sz = len(patern)
+
+cnt = 0
+for i in range (len(text) - pat_sz + 1):
+    if text[i : i + pat_sz] == patern:
+        cnt += 1
+        
+print(cnt)
