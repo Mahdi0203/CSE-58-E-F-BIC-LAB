fc310ddfdf607e15f5b50515b5329ab40be9277d
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2026-06-09 15:39:07 +0600

    Add function to find most common substrings
    Implement algorithm to find most frequent substrings of given length.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,27 @@
+text = input()
+k = int(input())
 
+txt_sz = len(text)
+
+mxOcc = 0
+for i in range(txt_sz - k + 1):
+    pat = text[i : i + k]
+    occ = 0
+    for j in range(txt_sz - k + 1) : 
+        if text[j : j + k] == pat :
+            occ += 1
+    if ( occ >= mxOcc ) :
+        mxOcc = occ
+
+patterns = []
+for i in range(txt_sz - k + 1):
+    pat = text[i : i + k]
+    occ = 0
+    for j in range(txt_sz - k + 1) : 
+        if text[j : j + k] == pat :
+            occ += 1
+    if ( occ == mxOcc ) :
+        patterns.append(pat)
+
+ans = list(set(patterns))
+print(ans)
