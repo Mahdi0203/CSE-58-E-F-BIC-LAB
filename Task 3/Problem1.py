9c067c0cb767c7ead4c7812281195fb40ab4361f
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2026-06-16 16:24:12 +0600

    Improve pattern matching with allowed differences
    Refactored pattern matching logic to count occurrences with up to 'd' differences.

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,43 @@
+text = input()
+
+k = int(input())
+d = int(input())
+
+szText = len(text)
+
+mxOcc = 0
+for i in range (szText - k + 1):
+    pat = text[i : i + k]
+    occ = 0
+    for j in range(szText - k + 1):
+        subText = text[j : j + k]
+        cnt = 0
+        for id in range(k):
+            if (pat[id] != subText[id]):
+                cnt += 1
+        if ( cnt <= d ) :
+            occ += 1
+    if ( occ >= mxOcc ):
+        mxOcc = occ
+
+patterns = []
+
+for i in range (szText - k + 1):
+    pat = text[i : i + k]
+    occ = 0
+    for j in range(szText - k + 1):
+        subText = text[j : j + k]
+        cnt = 0
+        for id in range(k):
+            if (pat[id] != subText[id]):
+                cnt += 1
+        if ( cnt <= d ) :
+            occ += 1
+    if ( occ >= mxOcc ):
+        patterns.append(pat)
+
+
+ans = list(set(patterns))
+
+print(ans)
 
