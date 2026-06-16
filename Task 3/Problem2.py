1684783c1a58323db2b1826c8cbeaa79f266237a
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2026-06-16 15:51:47 +0600

    Implement kmer matching with allowed mismatches
    Add functionality to find positions of kmers in a string with up to d mismatches.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,17 @@
+kmers = input()
+str = input()
 
+d = int(input())
+
+szPat = len(kmers)
+szStr = len(str)
+
+for i in range (0, szStr) :
+    cnt = 0
+    idx = i
+    for j in range(0, szPat, 1):
+        if ( kmers[j] != str[idx]) :
+            cnt += 1
+        idx += 1
+    if ( cnt <= d ) :
+        print(i)
